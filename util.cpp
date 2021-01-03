20752ec30d6afb6751be574a5f7f96423614c20f
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-03 00:28:45 +0000

    Day 1 - Removed unused files

--- util.cpp
+++
@@ -1,6 +0,0 @@
-#include <util.h>
-#include <stdlib.h>
-
-int Util::randRange(int min, int max) {
-    return rand();
-}
--- util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILITY_H
-#define UTILITY_H
-
-namespace Util {
-    int randRange(int min, int max);
-}
-
-#endif
